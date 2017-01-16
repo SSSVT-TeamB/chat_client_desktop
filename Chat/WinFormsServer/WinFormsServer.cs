f5642e63e2858ce0e16c37c5e90104f825a9f171
Erehtiaw <krysot5@gmail.com>
2017-01-17 00:13:51 +0100

    reworked client

---
+++ Chat/WinFormsServer/WinFormsServer.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignalRChat
+{
+    public partial class WinFormsServer : Form
+    {
+        private IDisposable SignalR { get; set; }
+        const string ServerURI = "http://localhost:8080";
+
+        internal WinFormsServer()
+        {
+            InitializeComponent();
+        }
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            WriteToConsole("Starting server...");
+            ButtonStart.Enabled = false;
+            Task.Run(() => StartServer());
+        }
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void StartServer()
+        {
+            try
+            {
+                SignalR = WebApp.Start(ServerURI);
+            }
+            catch (TargetInvocationException)
+            {
+                WriteToConsole("Server failed to start. A server is already running on " + ServerURI);
+                this.Invoke((Action)(() => ButtonStart.Enabled = true));
+                return;
+            }
+            this.Invoke((Action)(() => ButtonStop.Enabled = true));
+            WriteToConsole("Server started at " + ServerURI);
+        }
+        internal void WriteToConsole(String message)
+        {
+            if (RichTextBoxConsole.InvokeRequired)
+            {
+                this.Invoke((Action)(() =>
+                    WriteToConsole(message)
+                ));
+                return;
+            }
+            RichTextBoxConsole.AppendText(message + Environment.NewLine);
+        }
+
+        private void WinFormsServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            if (SignalR != null)
+            {
+                SignalR.Dispose();
+            }
+        }
+
+        private void WinFormsServer_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+        public override Task OnConnected()
+        {
+            Program.MainForm.WriteToConsole("Client connected: " + Context.ConnectionId);
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected()
+        {
+            Program.MainForm.WriteToConsole("Client disconnected: " + Context.ConnectionId);
+            return base.OnDisconnected();
+        }
+    }
+}
---
+++ Chat/WindowsFormsClient/WinFormsClient.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections;
+using System.Net.Http;
+using System.Windows.Forms;
+namespace WinFormsClient
+{
+    public partial class WinFormsClient : Form
+    {
+        private String UserName { get; set; }
+        private IHubProxy HubProxy { get; set; }
+        const string ServerURI = "http://localhost:8080/signalr";
+        private HubConnection Connection { get; set; }
+
+        ArrayList alOnlineUser = new ArrayList();
+
+        internal WinFormsClient()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            HubProxy.Invoke("Send", UserName, TextBoxMessage.Text);
+            TextBoxMessage.Text = String.Empty;
+            TextBoxMessage.Focus();
+        }
+
+        private async void ConnectAsync()
+        {
+            Connection = new HubConnection(ServerURI);
+            Connection.Closed += Connection_Closed;
+            HubProxy = Connection.CreateHubProxy("MyHub");
+            //Handle incoming event from server: use Invoke to write to console from SignalR's thread
+            HubProxy.On<string, string>("AddMessage", (name, message) =>
+                this.Invoke((Action)(() =>
+                    RichTextBoxConsole.AppendText(String.Format("{0}: {1}" + Environment.NewLine, name, message))
+                ))
+            );
+            try
+            {
+                await Connection.Start();
+            }
+            catch (HttpRequestException)
+            {
+                StatusText.Text = "Unable to connect to server: Start server before connecting clients.";
+                return;
+            }
+
+            //Activate UI
+            SignInPanel.Visible = false;
+            ChatPanel.Visible = true;
+            ButtonSend.Enabled = true;
+            TextBoxMessage.Focus();
+            RichTextBoxConsole.AppendText("Connected to server at " + ServerURI + Environment.NewLine);
+        }
+
+        private void Connection_Closed()
+        {
+            this.Invoke((Action)(() => ChatPanel.Visible = false));
+            this.Invoke((Action)(() => ButtonSend.Enabled = false));
+            this.Invoke((Action)(() => StatusText.Text = "You have been disconnected."));
+            this.Invoke((Action)(() => SignInPanel.Visible = true));
+        }
+
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            UserName = UserNameTextBox.Text;
+            if (!String.IsNullOrEmpty(UserName))
+            {
+                StatusText.Visible = true;
+                StatusText.Text = "Connecting to server...";
+                ConnectAsync();
+            }
+        }
+
+        private void WinFormsClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Connection != null)
+            {
+                Connection.Stop();
+                Connection.Dispose();
+            }
+        }
+
+        private void WinFormsClient_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Paint(object sender, PaintEventArgs e)
+        {
+            label2.Text = "You are connected as " + UserName;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
